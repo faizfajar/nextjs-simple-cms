8656542351bd984450887a626d8e85e4c947561e
faizfajar <faizaprianda112@gmail.com>
2025-07-12 16:37:12 +0700

    remove using menu context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import "../styles/globals.css";
 import { ReactNode } from "react";
 import { AuthProvider } from "../context/AuthContext";
-import { MenuProvider } from "../context/MenuContext";
-import PrivateRoute from "../components/PrivateRoute";
+import PrivateRoute from "@/components/PrivateRoute";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <AuthProvider>
-          <PrivateRoute>
-            <MenuProvider>{children}</MenuProvider>
-          </PrivateRoute>
+          <PrivateRoute>{children}</PrivateRoute>
         </AuthProvider>
       </body>
     </html>
